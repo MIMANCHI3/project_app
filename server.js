b124f1cea59acbb0d613fb5f1cb01949d38ad619
MIMANCHI3 <13739938114@163.COM>
2025-03-07 12:25:47 +0800

    server.js

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ app.get('/api/schedule', (req, res) => {
 app.post('/api/update', (req, res) => {
   const { table_id,week, day, status } = req.body;
   db.run(
-    `UPDATE schedule SET status = ? WHERE week = ? AND day = ?`,
+    `UPDATE schedule SET status = ? WHERE table_id = ? AND week = ? AND day = ?`,
     [status,table_id, week, day],
     (err) => {
       if (err) return res.status(500).send(err);
